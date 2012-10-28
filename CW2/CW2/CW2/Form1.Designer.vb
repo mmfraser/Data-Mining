71cc2a9aa31b34467f970a51b89aa11b9691550f
mmfraser <marc@coopersoftware.co.uk>
2012-10-28 15:18:05 +0000

    app

---
+++ CW2/CW2/CW2/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.gvData = New System.Windows.Forms.DataGridView()
+        Me.btnSetClassField = New System.Windows.Forms.Button()
+        Me.btnGenBins = New System.Windows.Forms.Button()
+        Me.btnSelectSS = New System.Windows.Forms.Button()
+        Me.fdSelectSS = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblClassField = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblNoBins = New System.Windows.Forms.Label()
+        Me.btnRandomiseRows = New System.Windows.Forms.Button()
+        Me.btnCalculateCorrelation = New System.Windows.Forms.Button()
+        CType(Me.gvData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'gvData
+        '
+        Me.gvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.gvData.Location = New System.Drawing.Point(13, 42)
+        Me.gvData.Name = "gvData"
+        Me.gvData.Size = New System.Drawing.Size(781, 399)
+        Me.gvData.TabIndex = 0
+        '
+        'btnSetClassField
+        '
+        Me.btnSetClassField.Location = New System.Drawing.Point(93, 13)
+        Me.btnSetClassField.Name = "btnSetClassField"
+        Me.btnSetClassField.Size = New System.Drawing.Size(96, 23)
+        Me.btnSetClassField.TabIndex = 1
+        Me.btnSetClassField.Text = "Set Class Field"
+        Me.btnSetClassField.UseVisualStyleBackColor = True
+        '
+        'btnGenBins
+        '
+        Me.btnGenBins.Location = New System.Drawing.Point(195, 13)
+        Me.btnGenBins.Name = "btnGenBins"
+        Me.btnGenBins.Size = New System.Drawing.Size(115, 23)
+        Me.btnGenBins.TabIndex = 2
+        Me.btnGenBins.Text = "Populate Bins"
+        Me.btnGenBins.UseVisualStyleBackColor = True
+        '
+        'btnSelectSS
+        '
+        Me.btnSelectSS.Location = New System.Drawing.Point(12, 13)
+        Me.btnSelectSS.Name = "btnSelectSS"
+        Me.btnSelectSS.Size = New System.Drawing.Size(75, 23)
+        Me.btnSelectSS.TabIndex = 3
+        Me.btnSelectSS.Text = "Select SS"
+        Me.btnSelectSS.UseVisualStyleBackColor = True
+        '
+        'fdSelectSS
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 453)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Selected Class Field: "
+        '
+        'lblClassField
+        '
+        Me.lblClassField.AutoSize = True
+        Me.lblClassField.Location = New System.Drawing.Point(127, 453)
+        Me.lblClassField.Name = "lblClassField"
+        Me.lblClassField.Size = New System.Drawing.Size(0, 13)
+        Me.lblClassField.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(192, 453)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "No Bins:"
+        '
+        'lblNoBins
+        '
+        Me.lblNoBins.AutoSize = True
+        Me.lblNoBins.Location = New System.Drawing.Point(246, 453)
+        Me.lblNoBins.Name = "lblNoBins"
+        Me.lblNoBins.Size = New System.Drawing.Size(0, 13)
+        Me.lblNoBins.TabIndex = 7
+        '
+        'btnRandomiseRows
+        '
+        Me.btnRandomiseRows.Location = New System.Drawing.Point(317, 13)
+        Me.btnRandomiseRows.Name = "btnRandomiseRows"
+        Me.btnRandomiseRows.Size = New System.Drawing.Size(111, 23)
+        Me.btnRandomiseRows.TabIndex = 8
+        Me.btnRandomiseRows.Text = "Randomise Rows"
+        Me.btnRandomiseRows.UseVisualStyleBackColor = True
+        '
+        'btnCalculateCorrelation
+        '
+        Me.btnCalculateCorrelation.Location = New System.Drawing.Point(435, 12)
+        Me.btnCalculateCorrelation.Name = "btnCalculateCorrelation"
+        Me.btnCalculateCorrelation.Size = New System.Drawing.Size(120, 23)
+        Me.btnCalculateCorrelation.TabIndex = 9
+        Me.btnCalculateCorrelation.Text = "Calculate Correlation"
+        Me.btnCalculateCorrelation.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(806, 475)
+        Me.Controls.Add(Me.btnCalculateCorrelation)
+        Me.Controls.Add(Me.btnRandomiseRows)
+        Me.Controls.Add(Me.lblNoBins)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblClassField)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSelectSS)
+        Me.Controls.Add(Me.btnGenBins)
+        Me.Controls.Add(Me.btnSetClassField)
+        Me.Controls.Add(Me.gvData)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.gvData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents gvData As System.Windows.Forms.DataGridView
+    Friend WithEvents btnSetClassField As System.Windows.Forms.Button
+    Friend WithEvents btnGenBins As System.Windows.Forms.Button
+    Friend WithEvents btnSelectSS As System.Windows.Forms.Button
+    Friend WithEvents fdSelectSS As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblClassField As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblNoBins As System.Windows.Forms.Label
+    Friend WithEvents btnRandomiseRows As System.Windows.Forms.Button
+    Friend WithEvents btnCalculateCorrelation As System.Windows.Forms.Button
+
+End Class
---
+++ CW2/CW2/CW2/Form1.vb
@@ -0,0 +1,236 @@
+﻿Public Class Form1
+
+    Private data As DataTable
+    Private classField As Integer
+    Private Const classColName As String = "Class"
+
+
+    Private Sub btnSelectSS_Click(sender As Object, e As EventArgs) Handles btnSelectSS.Click
+        fdSelectSS.ShowDialog()
+    End Sub
+
+
+    Private Sub fdSelectSS_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles fdSelectSS.FileOk
+        gvData.Columns.Clear()
+
+        Try
+            Dim oDb As New CooperSoftware.DataAccess.Database(CooperSoftware.DataAccess.DatabasePlatform.Excel)
+            oDb.Name = fdSelectSS.FileName
+            oDb.ExcelIMEX = 1
+            oDb.ExcelHeader = True
+            Dim oDal As New CooperSoftware.DataAccess.DAL(oDb)
+
+            data = oDal.GetDataTable("SELECT * FROM [Sheet1$]")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        
+        gvData.DataSource = data
+    End Sub
+
+
+    Private Sub btnSetClassField_Click(sender As Object, e As EventArgs) Handles btnSetClassField.Click
+        If gvData.SelectedCells.Count = 1 Then
+            classField = gvData.SelectedCells(0).ColumnIndex
+            lblClassField.Text = classField
+        Else
+            MsgBox("Only one column may be selected for the class field.")
+        End If
+    End Sub
+
+    Private Sub btnGenBins_Click(sender As Object, e As EventArgs) Handles btnGenBins.Click
+
+     
+        Dim noBins As Integer = CInt(InputBox("Please enter the number of bins"))
+
+        gvData.Columns.Add("Bin", "Bin")
+
+        Dim minValue As Double = (From row As DataGridViewRow In gvData.Rows Select CDbl(row.Cells(classField).Value)).Min
+
+        Dim maxValue As Double = (From row As DataGridViewRow In gvData.Rows Select CDbl(row.Cells(classField).Value)).Max
+
+   
+
+
+        Dim interval As Double = (maxValue - minValue) / noBins
+
+        lblNoBins.Text = noBins
+
+        For bin = 1 To noBins
+            For Each row As DataGridViewRow In gvData.Rows
+                Dim classValue As Double = CDbl(row.Cells(classField).Value)
+
+                If row.Cells("Bin").Value Is Nothing Then
+                    If classValue > ((bin - 1) * interval) AndAlso classValue <= (bin * interval) Then
+                        row.Cells("Bin").Value = bin
+                    ElseIf classValue = 0 Then
+                        row.Cells("Bin").Value = 1
+                    End If
+                End If
+
+                
+            Next
+        Next
+
+    End Sub
+
+    Private Sub btnRandomiseRows_Click(sender As Object, e As EventArgs) Handles btnRandomiseRows.Click
+        data = getDataTable(gvData)
+
+        Dim randomList As New List(Of DataRow)
+
+        Dim rowsAllocated As New List(Of Integer)
+        Dim noRows As Integer = gvData.Rows.Count
+        Dim randomNoGen As New Random
+
+        For index = 0 To noRows - 1
+            If rowsAllocated.Count <> noRows - 1 Then
+                ' Randomly generate index
+                Dim randomNo As Integer = randomNoGen.Next(0, (noRows - 1))
+
+                'Generate a new random no if row already allocated
+                While rowsAllocated.Contains(randomNo)
+                    randomNo = randomNoGen.Next(0, (noRows - 1))
+                End While
+
+                randomList.Add(data.Rows(randomNo))
+                rowsAllocated.Add(randomNo)
+
+            End If
+        Next
+
+        Dim newIndex As Integer = 0
+
+        Dim randomDataTable As New DataTable
+
+        For Each dtCol As DataColumn In data.Columns
+            randomDataTable.Columns.Add(dtCol.ColumnName)
+        Next
+
+        For index = 0 To randomList.Count - 1
+            randomDataTable.ImportRow(randomList(index))
+            index += 1
+        Next
+
+
+
+        ' gvData.Columns.Clear()
+        gvData.DataSource = randomDataTable
+        MsgBox("here")
+
+    End Sub
+
+    Public Function getDataTable(ByVal gv As DataGridView) As DataTable
+        Dim dt As New DataTable
+
+        ' Add columns
+        For Each col As DataGridViewColumn In gv.Columns
+            dt.Columns.Add(col.Name)
+        Next
+
+        ' Add data
+
+        For Each row As DataGridViewRow In gv.Rows
+            Dim newRow As DataRow = dt.NewRow
+
+            For Each cell As DataGridViewCell In row.Cells
+                newRow.Item(cell.ColumnIndex) = cell.Value
+            Next
+            dt.Rows.Add(newRow)
+        Next
+
+
+        Return dt
+    End Function
+
+    Public Function calcStdDev(ByVal numbers As Double()) As Double
+        Dim mean As Double = numbers.Sum / numbers.Length
+        Dim noVals As Integer = numbers.Length - 1
+        Dim sum As Double = 0
+
+        For Each num In numbers
+            sum += Math.Pow((num - mean), 2)
+        Next
+
+        Return Math.Sqrt((1 / noVals) * sum)
+    End Function
+
+    Public Function calcPearsonsR(ByVal f1 As Double(), ByVal f2 As Double()) As Double
+        If f1.Length <> f2.Length Then
+            Throw New Exception("Two fields must be same length (no of elements)")
+        End If
+
+        Dim noVals As Integer = f1.Length - 1
+
+        Dim meanF1 As Double = f1.Sum / f1.Length
+        Dim meanF2 As Double = f2.Sum / f2.Length
+
+        Dim stdDevF1 As Double = calcStdDev(f1)
+        Dim stdDevF2 As Double = calcStdDev(f2)
+
+        Dim sum As Double = 0
+        For i As Integer = 0 To noVals
+            sum += ((f1(i) - meanF1) / stdDevF1) * ((f2(i) - meanF2) / stdDevF2)
+
+        Next
+
+        Return (1 / noVals) * sum
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        'Dim f1(8) As Double
+        'Dim f2(8) As Double
+
+        'f1 = {0.36, 0.06, 0.87, 0.04, 0.45, 1, 0.23, 0.04, 0.28}
+        'f2 = {4, 1, 9, 1, 5, 10, 3, 1, 3}
+
+
+
+
+        'MsgBox(calcPearsonsR(f1, f2))
+    End Sub
+
+    Private Sub btnCalculateCorrelation_Click(sender As Object, e As EventArgs) Handles btnCalculateCorrelation.Click
+        Dim pctOfInstances As Integer = CInt(InputBox("Enter the number of instances (%) to account for"))
+        Dim noInstances As Integer = data.Rows.Count * (pctOfInstances / 100)
+
+        ' Generate the class field array
+        Dim index As Integer = 0
+        Dim classFieldVals(noInstances) As Double
+        For Each row As DataRow In data.Rows
+            classFieldVals(index) = row.Item(classColName)
+            index += 1
+            If index > noInstances Then
+                Exit For
+            End If
+
+        Next
+
+
+        For Each col As DataColumn In data.Columns
+            If col.ColumnName <> classColName Then
+                ' use this column.
+                ' get the top x% of rows and calculate Pearons r
+
+                ' Generate non class field array
+                Dim nonClassFieldVals(noInstances) As Double
+
+                index = 0
+                For Each row As DataRow In data.Rows
+                    nonClassFieldVals(index) = row.Item(col.ColumnName)
+                    index += 1
+                    If index > noInstances Then
+                        Exit For
+                    End If
+                Next
+
+                ' Calculate and store Pearsons r
+                MsgBox(calcPearsonsR(nonClassFieldVals, classFieldVals))
+            End If
+        Next
+
+
+
+    End Sub
+End Class
